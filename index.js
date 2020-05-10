1b0a5c9a0cf61c46606cdb90392d85c1f57524ae
Justin <justinphil02@yahoo.com>
2020-05-10 02:28:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 62/2 
+let num2 = 2 
+let multiply = num1 * num2 
 
-// Write your code here
+let random = Math.ceil(Math.random()*10000000)
+
+let mod = 40 % 6 
+
+let max = Math.max(1,2,3,4,5,20)
